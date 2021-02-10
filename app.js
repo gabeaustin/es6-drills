b96acae182daea2672d33fa3b5b5d0916bb3277d
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-02-10 15:44:40 -0600

    changed function favMovie to an arrow function

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-function favMovie(movie = "The Room", name = "Tommy Wiseau") {
+// Template Literals and Default Parameters Value
+// function favMovie(movie = "The Room", name = "Tommy Wiseau") {
+//     console.log(`My name is ${name} and my Favorite Movie is ${movie}.`);
+// }
+
+// Arrow Function
+favMovie = (movie = "The Room", name = "Tommy Wiseau") => 
     console.log(`My name is ${name} and my Favorite Movie is ${movie}.`);
-}
+
 
 favMovie("Saw", "Gabe");
 
