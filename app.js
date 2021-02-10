ef8eeda6fec6ebb46d8c729c6acc7c92bb0969cf
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-02-10 16:32:55 -0600

    added split statements on the getFirstName and getFirstNameConcise functions

--- app.js
+++ app.js
@@ -4,9 +4,24 @@
 // }
 
 // Arrow Function
-favMovie = (movie = "The Room", name = "Tommy Wiseau") => 
-    console.log(`My name is ${name} and my Favorite Movie is ${movie}.`);
+// let fullName = "Gabriel Austin";
+let favMovie = (movie = "The Room", name = "Tommy Wiseau") => console.log(`My name is ${name} and my Favorite Movie is ${movie}.`);
+
+let getFirstName = (fullName) => {
+    console.log(`Your first name is ${fullName.split(" ")[0]}.`);
+};
+
+let getFirstNameConcise = fullName => console.log(`${fullName.split(" ")[0]}, don't forget the "${fullName.split(" ")[1]}", is a great actor!`);
+
+
+
+
 
 
 favMovie("Saw", "Gabe");
+getFirstName("Gabriel Austin");
+getFirstNameConcise("Don Cheadle");
+
+
+
 
