30ea82f8a61db6bbc5c8477c7e5ddfbc4720b3cf
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-02-11 11:45:42 -0600

    added doMath function that gives exponent and product of two numbers

--- app.js
+++ app.js
@@ -1,31 +1,28 @@
-// Template Literals and Default Parameters Value
-// function favMovie(movie = "The Room", name = "Tommy Wiseau") {
-//     console.log(`My name is ${name} and my Favorite Movie is ${movie}.`);
-// }
-
-// Arrow Function
 let favMovie = (movie = "The Room", name = "Tommy Wiseau") => console.log(`My name is ${name} and my Favorite Movie is ${movie}.`);
 
 let getFirstName = (fullName) => {
     console.log(`Your first name is ${fullName.split(" ")[0]}.`);
 };
 
-let getFirstNameConcise = fullName => console.log(`${fullName.split(" ")[0]}, don't forget the "${fullName.split(" ")[1]}", is a great actor!`);
+let getFirstNameConcise = fullName => console.log(`Actor Don Cheadle's first name is ${fullName.split(" ")[0]}.`);
 
-/* COME BACK TO */
-/*
-let doMath = (num1 = 10, num2 = 5) => console.log(`The exponent result is ${(num1^num2)}. The product is ${(num1*num2)}.`);
-console.log(doMath);
-*/
+let doMath = (num1, num2) => ({
+    exponentResult: (num1^num2),
+    productResult: (num1*num2)
+});
 
+let result = doMath(5, 10);
+console.log(result);
 
 favMovie("Saw", "Gabe");
 getFirstName("Gabriel Austin");
 getFirstNameConcise("Don Cheadle");
 
-
 let nightOutArray = ["Ian", "Mexico", "Filet Mignon"];
+let firstName = "Gabriel";
 let nightOut = (name, location, favFood) => console.log(`${name} ${location} ${favFood}`);
+// let jobOpenings = () => let state = ()
+
 nightOut(...nightOutArray);
 
 
