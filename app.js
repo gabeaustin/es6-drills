ca0cc3df6aab76660590eccd5c993016769c561e
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-02-11 16:34:11 -0600

    started work on spread syntax

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+// Template Literals and Default Parameters Value
 let favMovie = (movie = "The Room", name = "Tommy Wiseau") => console.log(`My name is ${name} and my Favorite Movie is ${movie}.`);
 
+// Arrow Functions
 let getFirstName = (fullName) => {
     console.log(`Your first name is ${fullName.split(" ")[0]}.`);
 };
@@ -7,8 +9,8 @@ let getFirstName = (fullName) => {
 let getFirstNameConcise = fullName => console.log(`Actor Don Cheadle's first name is ${fullName.split(" ")[0]}.`);
 
 let doMath = (num1, num2) => ({
-    exponentResult: (num1^num2),
-    productResult: (num1*num2)
+    exponentResult: (num1 ^ num2),
+    productResult: (num1 * num2)
 });
 
 let result = doMath(5, 10);
@@ -18,24 +20,19 @@ favMovie("Saw", "Gabe");
 getFirstName("Gabriel Austin");
 getFirstNameConcise("Don Cheadle");
 
+
+// Spread Syntax
 let nightOutArray = ["Ian", "Mexico", "Filet Mignon"];
-let firstName = "Gabriel";
-let nightOut = (name, location, favFood) => console.log(`${name} ${location} ${favFood}`);
+
+let whereWeGoing = (...nightOutArray) => {};
+// let firstName = "Gabriel";
+// let nightOut = (name, location, favFood) => console.log(`${name} ${location} ${favFood}`);
 // let jobOpenings = () => let state = ()
 
-nightOut(...nightOutArray);
+// console.log(nightOutArray);
 
-
-
-
-
-let nums = [3,5,7];
-
-function addNums(a,b,c) {
-    console.log(a+b+c);    
-}
-
-addNums(...nums);
+whereWeGoing(...nightOutArray);
+console.log(nightOutArray);
 
 
 
